4d15cabd54c7d5fc05b5c9a5b17cd25185c9b3e2
Taylor Stemple <taylorstemple@gmail.com>
2018-03-19 21:25:51 -0700

    stretch

--- lab-taylor/src/main.js
+++ lab-taylor/src/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import './style/main.scss';
-import cowsay from 'cowsay-browser';
+import { say, DEFAULT, DOGE, DRAGON, HEDGEHOG, KITTY, VADER, TURTLE } from 'cowsay';
 import faker from 'faker';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -11,15 +11,30 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      content: '',
+      content: say({
+        text: 'Click dat button to generate some random text!',
+        cow: DEFAULT,
+      }),
+      
     }
+    this.cowSelect = this.cowSelect.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
     
   handleClick(e) {
     this.setState(state => {
-      return { content: faker.lorem.sentence() }
+      return { content: say({
+        text: faker.hacker.phrase(),
+        cow: this.state.cow
+      })
+      }
     });
+  }
+
+  cowSelect(e) {
+    this.setState({
+      cow: e.target.value
+    })
   }
 
   render() {
@@ -27,9 +42,20 @@ class App extends React.Component {
       <div>
         <h1>Generate Cowsay Lorem</h1>
         <button onClick={this.handleClick}>Click Me!</button>
-        <pre>{cowsay.say({
-          text: this.state.content
-        })}</pre>
+        <form>
+          <label>Select a "cow":
+            <select value={this.state.cow} onChange={this.cowSelect}>
+              <option value={DEFAULT}>Cow</option>
+              <option value={DOGE}>Doge</option>
+              <option value={DRAGON}>Dragon</option>
+              <option value={HEDGEHOG}>Hedgehog</option>
+              <option value={KITTY}>Kitty</option>
+              <option value={VADER}>Vader</option>
+              <option value={TURTLE}>I like turtles</option>
+            </select>
+          </label>
+        </form>
+        <pre>{ this.state.content }</pre>
       </div>
     )
   }
